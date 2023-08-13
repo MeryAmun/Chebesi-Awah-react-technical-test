ff30e5559dbe62e3ea49c9b26837216348dc9fc0
Chebs Awah <chebesiawah@gmail.com>
2023-08-13 08:43:42 +0100

    correction

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,38 @@
-'use client'
-import { useEffect,useState } from 'react'
-import ReviewCard from './components/ReviewCard'
-
+"use client";
+import { useEffect, useState } from "react";
+import ReviewCard from "./components/ReviewCard";
 
 export default function ReviewPage() {
-const [reviews, setReviews] = useState();
+  const [reviews, setReviews] = useState();
 
-    useEffect(() => {
-      const fetchData = async () => {  
+  useEffect(() => {
+    const fetchData = async () => {
       await fetch("data.json", {
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
-      }).then((response) => (response?.json()
-      )).then((data) => {
-        setReviews(data?.reviews)
       })
-      }
-      fetchData()
-        }, [])
+        .then((response) => response?.json())
+        .then((data) => {
+          setReviews(data?.reviews);
+        });
+    };
+    fetchData();
+  }, []);
 
   return (
-     <div className="main">
-      <div className='reviews'>
- <div className="reviews__header">
-   <h1>Reviews</h1>
-   </div>
-   <div className="reviews__container">
-   {
-    reviews?.map((review) => (
-      <ReviewCard key={review?.id} review={review}/>
-    ))
-   }
-   </div>
-</div>
-     </div>
-  )
+    <div className="main">
+      <div className="reviews">
+        <div className="reviews__header">
+          <h1>Reviews</h1>
+        </div>
+        <div className="reviews__container">
+          {reviews?.map((review) => (
+            <ReviewCard key={review?.id} review={review} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
